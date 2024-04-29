9f893945acc5ac0051ab12e69ad7c17a416dff98
TecHeReTiC3141 <talex73@mail.ru>
2024-04-29 17:28:13 +0300

    Site title fixed + problem is overlapping fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import ThemeSwitcher from "./ThemeSwitcher.tsx";
 export default function Header() {
     return (
         <div
-            className="w-full bg-red-500 sticky top-0 left-0 text-gray-900 dark:text-white py-4 px-6 md:px-8 flex gap-4 items-center z-30">
+            className="w-full bg-red-500 sticky top-0 left-0 text-gray-900 dark:text-white py-4 px-6 md:px-8 flex gap-4 items-center z-40">
             <Link to="/" className="flex items-center gap-4">
                 <img src={logo} alt="Logo" className="w-8 h-8"/>
                 <h2 className="text-sm md:text-xl hover:underline hidden sm:block">Кино справочник</h2>
